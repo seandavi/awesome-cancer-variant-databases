49ad32ba25492efed797dc3b96c187b2200946a4
Sean Davis <seandavi@gmail.com>
2017-05-24 16:43:09 -0400

    added ExAC

--- tools/get_data.mk
+++ tools/get_data.mk
@@ -1,6 +1,7 @@
 all_hg19: kaviar_hg19 \
 	clinvar_2.0_hg19 \
-	dbsnp_hg19
+	dbsnp_hg19 \
+	ExAC_nonTCGA_hg19
 
 kaviar_hg19: Kaviar-160204-Public-hg19.vcf.tar
 
@@ -16,3 +17,8 @@ dbsnp_hg19: 00-All.vcf.gz
 
 00-All.vcf.gz:
 	wget -N ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606_b150_GRCh37p13/VCF/00-All.vcf.gz
+
+ExAC_nonTCGA_hg19: ExAC_nonTCGA.r0.3.1.sites.vep.vcf.gz
+
+ExAC_nonTCGA.r0.3.1.sites.vep.vcf.gz:
+	wget -N ftp://ftp.broadinstitute.org/pub/ExAC_release/release0.3.1/subsets/ExAC_nonTCGA.r0.3.1.sites.vep.vcf.gz
